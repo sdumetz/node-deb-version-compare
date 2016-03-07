06008a93dbb1f628de431c150399df1791b5c5d3
Sebastien Dumetz <s.dumetz@holusion.com>
2016-03-07 16:00:09 +0100

    improve tests ouptuts

--- test/test_version.js
+++ test/test_version.js
@@ -9,12 +9,13 @@ function parse(){
    ,"1:1-2+3.4:5-nmu1":{epoch:1,upstream:"1-2+3.4:5",debian:"nmu1"}
    ,"2:1.2498-1":{epoch:2,upstream:"1.2498",debian:"1"}
   }
+  console.log("Parse versions")
   Object.keys(list).forEach(function(v){
     var version = new Version(v);
     assert(version.epoch == list[v].epoch,`version.epoch should be ${list[v].epoch}. ${version.epoch} found instead`);
     assert(version.upstream == list[v].upstream,`version.upstream should be ${list[v].upstream}. ${version.upstream} found instead`);
     assert(version.debian == list[v].debian,`version.debian should be ${list[v].debian}. ${version.debian} found instead`);
-    console.log("Parsed : ",v);
+    console.log("\t%s",v);
   });
 }
 
@@ -31,9 +32,10 @@ function compareStrings(){
   , ["1.0.0","1.0.0~rc1"]
   , ["1.2498","1.2492"]
   ]
+  console.log("compare version strings")
   list.forEach(function(e){
     assert(Version.prototype.compareStrings(e[0],e[1]) == 1,`Expect ${e[0]} to be greater than ${e[1]}`);
-    console.log("Compared : %s > %s",e[0],e[1])
+    console.log("\t%s > %s",e[0],e[1])
   })
 
 }
