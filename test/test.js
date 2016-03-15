449e39c2fdcb1a320fb11632c6cd92efd1091e4b
Sebastien Dumetz <s.dumetz@holusion.com>
2016-03-15 12:58:12 +0100

    update tests and made them less verbose

--- test/test.js
+++ test/test.js
@@ -12,6 +12,7 @@ const list = [ //item 1 is greater than item 2
 , ["2.0.0~rc2+u1","2.0.0~rc2"]
 , ["1.0.3~rc2+b2","1.0.3~rc2+b1"]
 , ["2.0.0","2.0.0~b1"]
+, ["2.0.0+u10","2.0.0+u9"]
 , ["2.21-9","2.19-18+deb8u3"]
 , ["2.21-9","2.19-18+deb8u3"]
 , ["2:1.2498-1","2:1.2492-4"]
@@ -20,5 +21,6 @@ console.log("Compare real world version numbers")
 list.forEach(function(el){
   assert(compare(el[0],el[1]) == 1,`Expected ${el[0]} to be greater than ${el[1]}. Got ${compare(el[0],el[1])}`);
   assert(compare(el[1],el[0]) == -1,`Expected ${el[0]} to be smaller than ${el[1]}. Got ${compare(el[1],el[0])}`);
-  console.log("\t%s > %s",el[0],el[1]);
+  //console.log("\t%s > %s",el[0],el[1]);
 });
+console.log("\t%s/%s OK",list.length,list.length);
--- test/test_version.js
+++ test/test_version.js
@@ -15,8 +15,10 @@ function parse(){
     assert(version.epoch == list[v].epoch,`version.epoch should be ${list[v].epoch}. ${version.epoch} found instead`);
     assert(version.upstream == list[v].upstream,`version.upstream should be ${list[v].upstream}. ${version.upstream} found instead`);
     assert(version.debian == list[v].debian,`version.debian should be ${list[v].debian}. ${version.debian} found instead`);
-    console.log("\t%s",v);
+    //console.log("\t%s",v);
   });
+  var length = Object.keys(list).length
+  console.log("\t%s/%s OK",length,length);
 }
 
 
@@ -25,22 +27,22 @@ function compareStrings(){
   const list = [ // first > second
     ["ad.1-0","ac.1"]
   , ["1.0.0","0.1.1"]
-  , ["1.11.0","0.1."]
+  , ["1.11.0","1.2.0"]
   , ["b","a"]
   , ["a","a~b"]
   , ["a","a~nmu1"]
   , ["1.0.0","1.0.0~rc1"]
   , ["1.2498","1.2492"]
   ]
-  console.log("compare version strings")
+  console.log("Compare version strings")
   list.forEach(function(e){
     r1 = Version.prototype.compareStrings(e[0],e[1])
     assert(r1 == 1,`Expect ${e[0]} to be greater than ${e[1]}. Got ${r1}`);
     r2 = Version.prototype.compareStrings(e[1],e[0])
     assert(r2 == -1,`Expect ${e[1]} to be smaller than ${e[0]}. Got ${r2} `);
-    console.log("\t%s > %s",e[0],e[1])
+    //console.log("\t%s > %s",e[0],e[1])
   })
-
+  console.log("\t%s/%s OK",list.length,list.length);
 }
 function test(){
   parse();
